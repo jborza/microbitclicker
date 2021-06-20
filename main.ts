3ed9232f796624d446d1d42b1e2574dc5047ecc6
Juraj Borza <jborza@gmail.com>
2021-06-20 10:38:47 +0200

    added level 2 generators

--- main.ts
+++ main.ts
@@ -1,19 +1,25 @@
 function tick () {
     points += generators * 1
+    generators += generators2 * 1
     showAll()
 }
 function showAll () {
     showNumber(0, points)
     showNumber(1, generators)
+    showNumber(2, generators2)
 }
 input.onButtonPressed(Button.A, function () {
     points += 1
     showNumber(0, points)
 })
 input.onButtonPressed(Button.B, function () {
+    if (generators > 10) {
+        generators += -10
+        generators2 += 1
+    }
     if (points >= 10) {
+        points += -10
         generators += 1
-        points = points - 10
     }
     showAll()
 })
@@ -28,10 +34,10 @@ for (let index = 0; index <= 4; index++) {
 }
 let brightness = 0
 let factor = 0
+let generators2 = 0
 let generators = 0
 let points = 0
-points = 0
-generators = 0
+music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
 basic.forever(function () {
 	
 })
